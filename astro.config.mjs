5d29f4d0af6f6686c23dd45c864ad2346e291103
ayoayco <ayo@ayco.io>
2024-08-17 12:31:02 +0200

    feat: show types of options on hover

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import node from "@astrojs/node";
 import serviceWorker from "./index.js";
 
 export default defineConfig({
-  // output: "server",
-  // adapter: node({
-  //   mode: "middleware"
-  // }),
+  output: "server",
+  adapter: node({
+    mode: "middleware"
+  }),
   site: 'https://ayo.ayco.io',
   integrations: [
     serviceWorker({
-      path: "./example_sw.js",
       assetCachePrefix: 'cozy-reader',
       customRoutes: [
         '/threads'
